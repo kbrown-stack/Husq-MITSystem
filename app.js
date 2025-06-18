8b4df2f091f3d951b319cde0515b2a890e40db59
Brown Kingsley <kbrownonuigbo@gmail.com>
2025-06-18 13:21:34 +0100

    Updated the App.js , installed new dependecies like cors to help my express app. implemented js file for the middleware

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit')
 
+
+
+// To Initialize Express
+const app = express();
+const PORT = process.env.PORT || 8080
+
+
+// Staring the application server 
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
